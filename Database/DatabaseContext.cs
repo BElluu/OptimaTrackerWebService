e0ec5577ad901f1249ce316e31053d71aa1ac07f
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2022-01-14 22:25:03 +0100

    Added migration, refactor and seed db

--- Database/DatabaseContext.cs
+++ Database/DatabaseContext.cs
@@ -13,55 +13,42 @@ namespace OptimaTrackerWebService.Database
         }
         public DbSet<Company> companies { get; set; }
         public DbSet<Event> events { get; set; }
-        public DbSet<EventDict> eventsDict { get; set; }
+        public DbSet<ProceduresDict> proceduresDict { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Company
             modelBuilder.Entity<Company>(c =>
             {
-                c.ToTable("Company");
-                c.Property(co => co.Id)
-                 .ValueGeneratedOnAdd()
-                 .UseIdentityColumn()
-                 .IsRequired();
-                c.HasIndex(co => co.SerialKey)
-                .IsUnique();
-                c.HasMany(e => e.Events)
-                .WithOne(co => co.Company)
-                .IsRequired();
+                c.ToTable("Companies");
+                c.Property(co => co.Id).ValueGeneratedOnAdd().UseIdentityColumn().IsRequired().HasColumnName("CompanyId");
+                c.HasIndex(co => co.SerialKey).IsUnique();
+                c.HasMany(e => e.Events).WithOne(co => co.Company).IsRequired();
             });
 
             // Event
 
             modelBuilder.Entity<Event>(e =>
             {
-                e.ToTable("Event");
-                e.HasOne(c => c.Company)
-                .WithMany(ev => ev.Events)
-                .HasForeignKey(c => c.CompanyId);
-                e.Property(ev => ev.ProcedureIdentity).HasColumnName("ProcedureId");
-                e.Ignore(ev => ev.ProcedureId);
+                e.ToTable("Events");
+                e.Property(ev => ev.Id).ValueGeneratedOnAdd().UseIdentityColumn().IsRequired().HasColumnName("EventId");
+                e.HasOne(c => c.Company).WithMany(ev => ev.Events).HasForeignKey(c => c.CompanyId);
+                e.Ignore(ev => ev.ProcedureName);
             });
 
             // Events Dict
 
-            modelBuilder.Entity<EventDict>(d =>
+            modelBuilder.Entity<ProceduresDict>(d =>
             {
-                d.ToTable("EventsDict");
-                d.HasData(
-                    new EventDict { Id = 1, ProcedureId = "Logowanie", ProcedureDescription = "Okno logowania" },
-                    new EventDict { Id = 2, ProcedureId = "BazLista", ProcedureDescription = "Okno listy baz danych" },
-                    new EventDict { Id = 3, ProcedureId = "KreatorBazy", ProcedureDescription = "Wizard bazy danych" },
-                    new EventDict { Id = 4, ProcedureId = "CfgStanowiskoOgolneParametry", ProcedureDescription = "Ustawienia ogolne stanowiska" },
-                    new EventDict { Id = 5, ProcedureId = "CfgSerwisOperacjiAutomatycznych", ProcedureDescription = "Ustawienia Serwisu Operacji Automatycznych" });
+                d.ToTable("ProceduresDict");
+                d.Property(d => d.Id).ValueGeneratedOnAdd().UseIdentityColumn().IsRequired().HasColumnName("ProcedureId");
             });
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql(configuration["ConnectionStrings:OptimaTrackerAppConnection"]);
+            optionsBuilder.EnableSensitiveDataLogging();
         }
-
     }
 }
---
+++ Migrations/20220114210647_InitMigration.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OptimaTrackerWebService.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SerialKey = table.Column<string>(type: "text", nullable: true),
+                    TIN = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProceduresDict",
+                columns: table => new
+                {
+                    ProcedureId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProcedureName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProceduresDict", x => x.ProcedureId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProcedureId = table.Column<int>(type: "integer", nullable: false),
+                    NumberOfOccurrences = table.Column<int>(type: "integer", nullable: false),
+                    CompanyId = table.Column<int>(type: "integer", nullable: false),
+                    TimeStamp = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                    table.ForeignKey(
+                        name: "FK_Events_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_SerialKey",
+                table: "Companies",
+                column: "SerialKey",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CompanyId",
+                table: "Events",
+                column: "CompanyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "ProceduresDict");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
